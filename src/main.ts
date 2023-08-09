f9cadf60cac8e7bbd8e3cecf2b1e22d2c09abe3b
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-09 12:18:51 +0600

    Add .js to export paths

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-export { PACKAGE_NAME } from './package-name';
-export { createContainer } from './create-container';
-export { saveNetworkStatus, type ISaveNetworkStatusOptions } from './save-network-status';
-export { uploadStatus, type IUploadStatusOptions } from './upload-status';
-export { getCurrentStatus, type NetworkStatusType } from './get-current-status';
-export { LinkName } from './link-name';
-export { NetworkStatus } from './components/network';
-export { useContainer } from './hooks/use-container';
-export { useNetworkStatus, type IUseNetworkStatusProps } from './hooks/use-network-status';
+export { PACKAGE_NAME } from './package-name.js';
+export { createContainer } from './create-container.js';
+export { saveNetworkStatus, type ISaveNetworkStatusOptions } from './save-network-status.js';
+export { uploadStatus, type IUploadStatusOptions } from './upload-status.js';
+export { getCurrentStatus, type NetworkStatusType } from './get-current-status.js';
+export { LinkName } from './link-name.js';
+export { NetworkStatus } from './components/network.js';
+export { useContainer } from './hooks/use-container.js';
+export { useNetworkStatus, type IUseNetworkStatusProps } from './hooks/use-network-status.js';
 
-import { createContainer } from './create-container';
-import { getCurrentStatus } from './get-current-status';
-import { saveNetworkStatus } from './save-network-status';
+import { createContainer } from './create-container.js';
+import { getCurrentStatus } from './get-current-status.js';
+import { saveNetworkStatus } from './save-network-status.js';
 
-export {Package,PackageOptions} from './package'
+export {Package,PackageOptions} from './package.js'
 
 export const Network = {
     createContainer,
